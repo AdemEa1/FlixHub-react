ed46c4f8c6d7de52c81135e9251009eb2f12323d
Adem <ademet123676@icloud.com>
2023-01-17 18:17:15 +1100

    update

--- src/components/MovieHome.jsx
+++ src/components/MovieHome.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
